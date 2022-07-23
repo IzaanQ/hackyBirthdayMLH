0db98b4be799e8fc5874d369e4a363b976c77d0d
izaan Qaiser <izaanqaiser05@gmail.com>
2022-07-23 01:53:15 -0400

    collision detection (rectangles)

--- index.js
+++ index.js
@@ -36,23 +36,26 @@ class Player {
     }
 }
 
-const Platform { 
+class Platform { 
     constructor() {
         this.position = {
-            x: 0,
-            y: 0
+            x: 200,
+            y: 300
         }
         this.width = 200
-        this.width = 200
+        this.height = 20
 }
     
 draw() {
-    c.fillRect()
+    c.fillStyle = 'blue'
+    c.fillRect(this.position.x, this.position.y, this.width, this.height)
 }
 
 }
 
 const player = new Player()
+const platform = new Platform()
+
 const keys = {
   right: {
     pressed: false,
@@ -67,6 +70,7 @@ function animate() {
     requestAnimationFrame(animate)
     c.clearRect(0,0, canvas.width, canvas.height)
     player.update()
+    platform.draw()
 
     if (keys.right.pressed) {
         player.velocity.x = 5
@@ -78,6 +82,10 @@ function animate() {
 
     else { 
         player.velocity.x = 0
+    }
+
+    if (player.position.y + player.height <= platform.position.y && player.position.y + player.height + player.velocity.y >= platform.position.y && player.position.x + player.width >= platform.position.x && player.position.x <= platform.position.x + platform.width) {
+        player.velocity.y = 0 
     }
 }
 
