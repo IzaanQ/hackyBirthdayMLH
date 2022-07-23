a8ee26e94a1336598f889db1b55f85a63a98335f
izaan Qaiser <izaanqaiser05@gmail.com>
2022-07-22 22:10:34 -0400

    studf

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
 
-console.log('canvas')
+class Player { 
+    constructor() { 
+        this.position = {
+            x: 100,
+            y: 100
+        }
+        this.width = 100
+        this.height = 100
+    }
+    draw() { 
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
+
+const player = new Player()
+player.draw()
