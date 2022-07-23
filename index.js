d95224daf1223cf42a69a4a0cbb83b7a33aed0de
izaan Qaiser <izaanqaiser05@gmail.com>
2022-07-22 22:15:43 -0400

    velocity

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
+
+canvas.width = innerWidth
+canvas.height = innerHeight
+
+
 class Player { 
     constructor() { 
         this.position = {
             x: 100,
             y: 100
         }
-        this.width = 100
-        this.height = 100
+        this.velocity = {
+            x: 0,
+            y: 1
+        }
+        this.width = 30
+        this.height = 30
     }
     draw() { 
+        c.fillStyle = 'red'
         c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+
+    update() { 
+        this.draw()
+        this.position.y += this.velocity.y
     }
 }
 
